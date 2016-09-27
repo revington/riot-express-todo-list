428998de61cd3ef930632ced6fc83db996d3368f
Pedro Narciso García Revington <p.revington@gmail.com>
2016-09-27 15:33:44 +0200

    make it simple

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require('express'),
     api = require('./api'),
     app = express(),
     bodyParser = require('body-parser');
+
+// Set the views directory and template engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+// Set our static directory for public assets like client scripts
 app.use(express.static('public'));
+
+// Parses the body on incoming requests
 app.use(bodyParser.json());
+
+// Pretty prints HTML output
 app.locals.pretty = true;
 app.get('/', function (req, res) {
     res.render('index');
@@ -16,10 +24,14 @@ app.get('/tags/:name.tag', function (req, res) {
     //res.type('text/plain');
     res.render('../client/' + name);
 });
+
+// Mount the api sub application
+app.use('/api/todos/', api);
+
+// Start listening for connections
 app.listen(3000, function (err) {
     if (err) {
         console.error('Cannot listen at port 3000', err);
     }
     console.log('Todo app listening at port 3000');
 });
-app.use('/api/todos/', api);
